7f7c3f4382076be26539ac0fb8df930dfb59cf5d
lucastort1 <lucas.tort@sou.inteli.edu.br>
2024-06-14 11:22:54 -0300

    correcaoCodigo
    Correçâo do código Heroes Controller

--- api/controllers/HeroesController.js
+++ api/controllers/HeroesController.js
@@ -1,8 +1,24 @@
 module.exports = {
-  list: async (req, res) => {
-    const heroes = await Hero.find();
-    return res.json(heroes);
-  },
+  atributtes: {
+    name: {
+      type: 'string',
+      required: true
+    },
+    age: {
+      type: 'number',
+    },
+    superPower: {
+      type: 'string',
+    },
+    list: async (req, res) => {
+      try {
+        const heroes = await Hero.find();
+        return res.json(heroes);
+      } catch (err) {
+        return res.serverError(err);
+      }
+    },
+    
   listwithgun: async (req, res) => {
     try {
       const query = "SELECT * FROM hero INNER JOIN gun ON hero.id = gun.owner";
@@ -17,12 +33,13 @@ module.exports = {
       return res.serverError(err);
     }
   },
+
   create: async (req, res) => {
     try {
-      const hero = await Hero.new(req.body).fetch();
+      const hero = await Hero.create(req.body).fetch();
       return res.json(hero);
     } catch (err) {
       return res.serverError(err);
     }
   },
-};
+  };
